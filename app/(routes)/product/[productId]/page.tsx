f9fef775aafba9269afc943337e726ed6b7c65f8
vincent.thet <naingoted@gmail.com>
2023-07-10 00:23:08 +0800

    [deployment] update constant

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,21 +1,18 @@
-import Gallery from '@/components/gallery';
-import Info from '@/components/info';
-import getProduct from '@/actions/get-product';
-import Container from '@/components/ui/container';
+import Gallery from "@/components/gallery";
+import Info from "@/components/info";
+import getProduct from "@/actions/get-product";
+import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 interface ProductPageProps {
   params: {
     productId: number;
-  },
+  };
 }
 
-const ProductPage: React.FC<ProductPageProps> = async ({ 
-  params
- }) => {
+const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
   const product = await getProduct(params.productId);
-
 
   if (!product) {
     return null;
@@ -26,15 +23,15 @@ const ProductPage: React.FC<ProductPageProps> = async ({
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            <Gallery images={product.images} />
+            <Gallery images={product?.images} title={product?.title} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
               <Info data={product} />
             </div>
           </div>
         </div>
       </Container>
-    </div>  
-  )
-}
+    </div>
+  );
+};
 
 export default ProductPage;
